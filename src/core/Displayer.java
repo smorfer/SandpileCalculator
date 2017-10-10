eb69aab4b5a1e2ba9cfc98985748e9174f112f83
smorfer <smorfi98@gmail.com>
2017-10-10 08:38:34 +0200

    Remove the textlabels, as they were senseless.

--- src/core/Displayer.java
+++ src/core/Displayer.java
@@ -19,17 +19,15 @@ public class Displayer extends Application{
     static double offsetX, offsetY;
     static double scale;
     static int amount;
-    static Label[] labels;
     static Rectangle[] rectangles;
     public static void main(String[] args) {
-        amount = 10000;
-        displayer = new SandPileGrid(99,0);
+        amount = 100000;
+        displayer = new SandPileGrid(199,0);
         displayer.setPile(displayer.sidelength/2,displayer.sidelength/2,amount);
-        scale = 5;
+        scale = 3;
         offsetX = (displayer.sidelength / 2) * (-scale);
         offsetY = offsetX;
-        labels = new Label[displayer.sandpiles.length];
-        rectangles = new Rectangle[labels.length];
+        rectangles = new Rectangle[displayer.sandpiles.length];
         launch(args);
     }
 
@@ -41,25 +39,19 @@ public class Displayer extends Application{
         Progress.setText("Progressed "+ (amount-displayer.getSandpile(displayer.sidelength/2,displayer.sidelength/2))+ " of "+amount);
         Button StartButton = new Button("Start");
         Button StopButton = new Button("Stop");
-        Button SwitchView = new Button("Switch View");
         primaryStage.setTitle("Sandpile Displayer");
         BorderPane layout = new BorderPane();
 
-        Group fields = new Group();
         Group colourFields = new Group();
-        for (int i = 0; i < labels.length; i++){
-            labels[i] = new Label();
-            fields.getChildren().add(labels[i]);
+        for (int i = 0; i < rectangles.length; i++){
             rectangles[i] = new Rectangle(scale,scale, Color.BLUE);
             colourFields.getChildren().add(rectangles[i]);
         }
 
-        Group center = new Group(fields,colourFields);
+        Group center = new Group(colourFields);
 
-        fields.setVisible(false);
         layout.setCenter(center);
         layout.setTop(new VBox(10,StartButton, StopButton,Progress));
-        layout.setBottom(SwitchView);
 
         Scene CalculatorScene = new Scene(layout);
 
@@ -68,7 +60,7 @@ public class Displayer extends Application{
         primaryStage.setMaximized(true);
 
 
-        for (int i = 0; i < labels.length; i++){
+        for (int i = 0; i < rectangles.length; i++){
             if(i!=0){
                 if (i % displayer.sidelength == 0){
                     offsetY += scale;
@@ -78,16 +70,13 @@ public class Displayer extends Application{
                 }
             }
 
-            fields.getChildren().get(i).setLayoutY(offsetY);
-            fields.getChildren().get(i).setLayoutX(offsetX);
             colourFields.getChildren().get(i).setLayoutX(offsetX);
             colourFields.getChildren().get(i).setLayoutY(offsetY);
             System.out.println("Position Sandpile No. "+i);
         }
 
-        for (int i = 0; i < labels.length; i++){
-            labels[i].setText(Integer.toString(displayer.sandpiles[i].getSandpile()));
-            rectangles[i].setFill(setColour(Integer.parseInt(labels[i].getText())));
+        for (int i = 0; i < rectangles.length; i++){
+            rectangles[i].setFill(setColour(displayer.sandpiles[i].getSandpile()));
         }
 
         AnimationTimer timer = new AnimationTimer() {
@@ -95,11 +84,10 @@ public class Displayer extends Application{
             public void handle(long now) {
                 Progress.setText("Progressed "+ displayer.getSandpile(displayer.sidelength/2,displayer.sidelength/2)+ " of "+amount);
                 displayer.tobble();
-                for (int i = 0; i < labels.length; i++){
-                    labels[i].setText(Integer.toString(displayer.sandpiles[i].getSandpile()));
-                    rectangles[i].setFill(setColour(Integer.parseInt(labels[i].getText())));
+                for (int i = 0; i < rectangles.length; i++){
+                    rectangles[i].setFill(setColour(displayer.sandpiles[i].getSandpile()));
                 }
-                for (int i = 0; i < labels.length; i++){
+                for (int i = 0; i < rectangles.length; i++){
                     if(i!=0){
                         if (i % displayer.sidelength == 0){
                             offsetY += scale;
@@ -109,8 +97,7 @@ public class Displayer extends Application{
                         }
                     }
 
-                    fields.getChildren().get(i).setLayoutY(offsetY);
-                    fields.getChildren().get(i).setLayoutX(offsetX);
+
                     colourFields.getChildren().get(i).setLayoutX(offsetX);
                     colourFields.getChildren().get(i).setLayoutY(offsetY);
                     colourFields.getChildren().get(i).resize(scale,scale);
@@ -121,13 +108,10 @@ public class Displayer extends Application{
         primaryStage.show();
         StartButton.setOnAction(event -> timer.start());
         StopButton.setOnAction(event -> timer.stop());
-        SwitchView.setOnAction(event -> {
-            fields.setVisible(!fields.isVisible());
-            colourFields.setVisible(!colourFields.isVisible());
-        });
+
     }
 
-    public Color setColour(int def){
+    private Color setColour(int def){
         switch (def){
             case 0:
                 return Color.BLACK;
