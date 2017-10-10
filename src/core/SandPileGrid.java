16a468fc04feea34472aae9057dc672eb523d36a
smorfer <smorfi98@gmail.com>
2017-10-10 09:04:32 +0200

    Remove the deprecated constructor, as the algorithmic constructor works without problems.

--- src/core/SandPileGrid.java
+++ src/core/SandPileGrid.java
@@ -10,37 +10,8 @@ import java.util.Scanner;
  */
 public class SandPileGrid {
 
-    Sandpile uplft,upmid,uprght;
-    Sandpile midlft,midmid,midrght;
-    Sandpile dwnlft,dwnmid,dwnrght;
-
     Sandpile[] sandpiles;
     int sidelength;
-
-    @Deprecated
-    public SandPileGrid(int ul,int um,int ur,int ml,int mm,int mr, int dl, int dm, int dr) {
-        uplft = new Sandpile(ul);
-        upmid = new Sandpile(um);
-        uprght = new Sandpile(ur);
-        midlft = new Sandpile(ml);
-        midmid = new Sandpile(mm);
-        midrght = new Sandpile(mr);
-        dwnlft = new Sandpile(dl);
-        dwnmid = new Sandpile(dm);
-        dwnrght = new Sandpile(dr);
-
-        uplft.setRelations(new GridBoundary(),midlft,upmid,new GridBoundary());
-        upmid.setRelations(new GridBoundary(),midmid,uprght,uplft);
-        uprght.setRelations(new GridBoundary(),midrght,new GridBoundary(),upmid);
-        midlft.setRelations(uplft,dwnlft,midmid,new GridBoundary());
-        midmid.setRelations(upmid,dwnmid,midrght,midlft);
-        midrght.setRelations(uprght,dwnrght,new GridBoundary(),midmid);
-        dwnlft.setRelations(midlft,new GridBoundary(),dwnmid,new GridBoundary());
-        dwnmid.setRelations(midmid,new GridBoundary(),dwnrght,dwnlft);
-        dwnrght.setRelations(midrght,new GridBoundary(),new GridBoundary(),dwnmid);
-
-        sandpiles = new Sandpile[]{uplft,upmid,uprght,midlft,midmid,midrght,dwnlft,dwnmid,dwnrght};
-    }
 
     public SandPileGrid(int sideLength, int fill){
         this.sidelength = sideLength;
@@ -94,7 +65,6 @@ public class SandPileGrid {
             for (Sandpile s : overflowing){
                 s.collapse();
             }
-            //tobblePrioritized(checkGridOverflow());
 
         }
 
